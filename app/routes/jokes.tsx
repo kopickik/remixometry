9ff0ff351fb03fb676fa66a42e3ba5547363f614
Kyle Kopicki <kyle.kopicki@datalinksoftware.com>
2021-12-02 17:29:29 -0500

    add db

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,52 +1,74 @@
-import { Link, LinksFunction } from "remix";
-import { Outlet } from "remix";
-import stylesUrl from "../styles/jokes.css";
+import {
+  Link,
+  LinksFunction,
+  LoaderFunction,
+  Outlet,
+  useLoaderData,
+} from "remix"
+import { db } from "~/utils/db/server"
+import stylesUrl from "../styles/jokes.css"
 
 export const links: LinksFunction = () => {
-    return [
-      {
-        rel: "stylesheet",
-        href: stylesUrl
-      }
-    ];
-  };
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ]
+}
+
+type LoaderData = {
+  jokeListitems: Array<{ id: string; name: string }>
+}
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokeListitems: await db.joke.findMany({
+      take: 5,
+      select: { id: true, name: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  }
+
+  return data
+}
 
 export default function JokesRoute() {
-    return (
-        <div className="jokes-layout">
-            <header className="jokes-header">
-                <div className="container">
-                    <h1 className="home-link">
-                        <Link
-                            to="/"
-                            title="Jokes"
-                            aria-label="Jokes"
-                        >
-                            <span className="logo">🤪</span>
-                            <span className="logo-medium">J🤪KES</span>
-                        </Link>
-                    </h1>
-                </div>
-            </header>
-            <main className="jokes-main">
-                <div className="container">
-                    <div className="jokes-list">
-                        <Link to=".">Get a random joke</Link>
-                        <p>Here are a few more jokes to check out:</p>
-                        <ul>
-                            <li>
-                                <Link to="some-joke-id">Hippo</Link>
-                            </li>
-                        </ul>
-                        <Link to="new" className="button">
-                            Add your own
-                        </Link>
-                    </div>
-                    <div className="jokes-outlet">
-                        <Outlet />
-                    </div>
-                </div>
-            </main>
+  const data = useLoaderData<LoaderData>()
+
+  return (
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Jokes" aria-label="Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
         </div>
-    )
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              {data.jokeListitems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
